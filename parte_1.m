09d4c8488a7cb225698c4469460eebe0eb3717c3
Patricia <paty4paty@gmail.com>
2020-09-09 00:47:37 -0300

    Parte 1.1 según profe

--- parte_1.m
+++ parte_1.m
@@ -9,38 +9,31 @@
 % Estas son de la forma H1=a/(s+b) y H2=c/(s+d)
 % Se solicita ingresar a, b, c y d
 disp("La función de transferencia de H1 es de la forma: a/(s+b")
-a = input("Ingrese a: ")
-b = input("Ingrese b: ")
+a = input("Ingrese a: ");
+b = input("Ingrese b: ");
 
 disp("La función de transferencia de H2 es de la forma: c/(s+d")
-c = input("Ingrese c: ")
-d = input("Ingrese d: ")
+c = input("Ingrese c: ");
+d = input("Ingrese d: ");
 
-% Pasando las funciones de transferencia H1 y H2 en retroalimentación a
-% solo una función H(s). Luego se separa H(s) en serie dejando el nuevo H1
-% como 1/denominador y el nuevo H2 como el numerador
+% H1 y H2 están en retroalimentación, y utilizando Masón se tiene:
+% X1=4(U-X2)/(s+5) y X2=2(X1)/(s+1), donde X1 es la salida de H1 y X2 la
+% salida de H2
 
-% Se aplica la inversa de Laplace al nuevo H1
-% Obteniendo la ecucación diferencial:
-% z''(t) + (d+b)z'(t) + (bd+ac)z(t) = u(t)  Con z como salida de H1
-% Con esto definimos las variables de estado
-% x1=z, x2=z'
-% Obteniendo primeras derivadas
-% x1'=x2, x2'=u-(d+b)x2-(bd+ac)x1
-% Escrito de manera matricial -> X'=AX+BU
-A_H1=[0 1;-(b*d+a*c) -(d+b)]
-B_H1=[0;1]
+% Despejando sX1 y sX2 se obtiene:
+% sX1=aU-aX2-bX1
+% sX2=cX1-X2
 
-% z es la salida de H1 y a la vez la entrada de H2
-% De esta forma se tiene: z->as+ad->y
-% Se aplica la inversa de Laplace al nuevo H2
-% Obteniendo la ecucación diferencial:
-% y(t) = az'(t)+adz(t)
-% Con esto definimos las variables de estado
-% x1=z, x2=z'
-% y=adx1+ax2
-% Escrito de manera matricial -> Y=CX+DU
-C_H2=[a*d a]
-D_H2=[0;0]
+% Volviendo al tiempo (inversa de Laplace)
+% x1'(t)=au(t)-ax2(t)-bx1(t)
+% x2'(t)=cx1(t)-x2(t)
+% Además, la salida Y=X1
+% Matricialmente: X'=AX+BU    Y=CX+DU
+A=[-b -a; c -d];
+B=[a; 0];
+C=[1 0];
+D=0;
+
+
 
 
