154d69e926b3214d01383d48ae02305baa0045b6
Gustavo H <gustavo.hurtado@usach.cl>
2020-09-13 19:36:17 -0300

    Lab terminado

--- parte_2_1.m
+++ parte_2_1.m
@@ -1,5 +1,8 @@
-syms h1 h2
+syms h1 h2 F1 F2 A
 A = input("Ingrese área recipiente 1 y 2: ");
+F1 = input("Ingrese flujo inicial recipiente 1: ");
+h1 = input("Ingrese nivel recipiente 1: ");
+h2 = input("Ingrese nivel recipiente 2: ");
 
 %Definición de modelo fenomenológico
 
@@ -11,19 +14,19 @@ F2 = h2;
 dv1 = F1 - F1;
 dv2 = F1 - F2;
 
-%Volumen
+%Volumen de agua
 V1 = A*h1;
 V2= A*h2;
 
 %Variables
 
-%Entradas -> F1 (u)
-%F1 = u
-%Salidas -> h1 y h2 (y1 ; y2)
-%Estado -> h1 y h2 (x1 ; x2)
+disp("Entradas -> F1 (u)")
+disp("Salidas -> h1 y h2 (y1 ; y2)")
+disp("Estado -> h1 y h2 (x1 ; x2)")
+
 x1 = h1;
 x2 = h2;
-
+u = F1;
 %Modelo estado
 %X. = AX + BU
 %Y = CX + DU
@@ -33,18 +36,31 @@ x2 = h2;
 dh1 = 0;
 dh2 = (h1 - 2*h2)/A;
 
+disp("Las variables de estado son:")
+disp("dx1 = 0")
+disp("dx2 = x1/A - 2*x2/A")
+
 %Se obtienen las variables de estado
-dx1 = 0*x1 + 0*x2;
-dx2 = (x1 - 2*x2)/A;
+disp("Variables de estado evaluadas:")
+dx1 = 0*x1 + 0*x2
+dx2 = (x1 - 2*x2)/A
 
-
-A = [0 0 ; ((dx2 + 2*x2./A)./x1) ((dx2 - x1./A)./x2)]
+disp("Se generan las matrices para el modelo de estado")
+%Se generan las matrices A, B, C y D para %X. = AX + BU  ; Y = CX + DU
+disp("Matriz A:")
+A = [0 0 ; ((dx2 + 2*x2/A)/x1) ((dx2 - x1/A)/x2)]
+disp("Matriz B:")
 B = [0 ; 0]
+disp("Matriz C:")
 C = [1 0; 0 1]
+disp("Matriz D:")
 D = [0;0]
 
-x0 = [1 ; 0];
+disp("** Presione enter para graficar comportamiento del modelo de estado **")
+input("")
+disp("Generando gráfico ...")
 
+x0 = [1 ; 0];
 sys = ss(double(A),double(B),double(C),double(D));
 initial(sys,x0)
 
--- parte_2_2.m
+++ parte_2_2.m
@@ -1,64 +1,79 @@
-syms h1 h2 F0 u
+syms l1 l2 F0 u A1 A2 R1 R2
+F0 = input("Ingrese flujo inicial recipiente 1: ");
 A1 = input("Ingrese área recipiente 1: ");
 A2 = input("Ingrese área recipiente 2: ");
 R1 = input("Ingrese R recipiente 1: ");
 R2 = input("Ingrese R recipiente 2: ");
+l1 = input("Ingrese nivel recipiente 1: ");
+l2 = input("Ingrese nivel recipiente 2: ");
 
 %Definición de modelo fenomenológico
 
 %Flujos
-F1 = (h1 - h2)/R1;
-F2 = h2/R2;
+F1 = (l1 - l2)/R1;
+F2 = l2/R2;
 
 %Variación de volumen
 dv1 = F0 - F1;
 dv2 = F1 - F2;
 
 %Volumen
-V1 = A1*h1;
-V2= A2*h2;
+V1 = A1*l1;
+V2= A2*l2;
 
 %Variables
+x1 = l1;
+x2 = l2;
+u = F0;
 
-%Entradas -> F1 (u)
-%F0 = u;
-%Salidas -> h1 y h2 (y1 ; y2)
-%Estado -> h1 y h2 (x1 ; x2)
-%x1 = h1;
-%x2 = h2;
+disp("Entradas -> F0 (u)")
+disp("Salidas -> l1 y l2 (y1 ; y2)")
+disp("Estado -> l1 y l2 (x1 ; x2)")
 
 %Modelo estado
 %X. = AX + BU
 %Y = CX + DU
 
+
+
 %Uniendo las ecuaciones del modelo fenomenológico se obtiene:
-%Dado que dv1 = 0 => dh1 = 0
-dh1 = F0/A1 - (h1 - h2)/(R1*A1);
-dh2 = (h1 - h2)/(R1*A2) - (h2)/(R2*A2);
+%Dado que dv1 = 0 => dl1 = 0
+dl1 = F0/A1 - (l1 - l2)/(R1*A1);
+dl2 = (l1 - l2)/(R1*A2) - (l2)/(R2*A2);
 
-x1 = h1;
-x2 = h2;
-u = F0;
+disp("Las variables de estado son:")
+disp("dx1 = -(x1)/(R1*A1) + x2/(A1*R2) + u/A1")
+disp("dx2 = (x1)/(R1*A2) - x2/(A2*(R1+R2)) + 0*u")
 
+disp("Variables de estado evaluadas:")
 %Se obtienen las variables de estado
-dx1 = -(x1)/(R1*A1) + x2/(A1*R2) + u/A1; 
-dx2 = (x1)/(R1*A2) - x2/(A2*(R1+R2)) + 0*u;
+dx1 = -(x1)/(R1*A1) + x2/(A1*R2) + u/A1
+dx2 = (x1)/(R1*A2) - x2/(A2*(R1+R2)) + 0*u
 
-mv_1 = (dx1 - x2/(A1*R2) - u/A1)/x1
-mv_2 = (dx1 + (x1)/(R1*A1) - u/A1)/x2
-mv_3 = (dx2 + x2/(A2*(R1+R2)))/x1
-mv_4 = (dx2 - x1/(R1*A2))/x2
+%Se realizan operaciones convenientes para obtener los valores de A y B.
+mv_1 = (dx1 - x2/(A1*R2) - u/A1)/x1;
+mv_2 = (dx1 + (x1)/(R1*A1) - u/A1)/x2;
+mv_3 = (dx2 + x2/(A2*(R1+R2)))/x1;
+mv_4 = (dx2 - x1/(R1*A2))/x2;
+mv_5 =  (dx1 - (-(x1)/(R1*A1) + x2/(A1*R2)))/u;
+mv_6 =  (dx2 - ((x1)/(R1*A2) - x2/(A2*(R1+R2))))/u;
 
-mv_5 =  (dx1 - (-(x1)/(R1*A1) + x2/(A1*R2)))/u
-mv_6 =  (dx2 - ((x1)/(R1*A2) - x2/(A2*(R1+R2))))/u
-
+disp("Se generan las matrices para el modelo de estado")
+%Se generan las matrices A, B, C y D para %X. = AX + BU  ; Y = CX + DU
+disp("Matriz A:")
 A = [mv_1 mv_2 ; mv_3 mv_4]
+disp("Matriz B:")
 B = [mv_5 ; mv_6]
+disp("Matriz C:")
 C = [1 0; 0 1]
+disp("Matriz D:")
 D = [0;0]
 
-x0 = [100 ; 0];
+disp("** Presione enter para graficar comportamiento del modelo de estado **")
+input("")
+disp("Generando gráfico ...")
 
+x0 = [1 ; 0];
 sys = ss(double(A),double(B),double(C),double(D));
 initial(sys,x0)
 
